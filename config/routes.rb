a07f98465f026714b25c80ff9298e551af1dda0c
Ernest Ho <ernest.heh@gmail.com>
2023-03-09 15:48:10 +0800

    added routes for bookmarks, generated bookmarks controller with new action

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :lists, only: %i[index show new create]
+  resources :lists, only: %i[index show new create] do
+    resources :bookmarks, only: %i[new create destroy]
+  end
+
 end
 
 
@@ -14,3 +17,7 @@ end
 # A user can create a new list
 # GET "lists/new"
 # POST "lists"
+
+# GET "lists/42/bookmarks/new"
+# POST "lists/42/bookmarks"
+# DELETE "bookmarks/25"
