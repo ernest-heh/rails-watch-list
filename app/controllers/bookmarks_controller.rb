b87996a6116a8c085e63007a9e9c5ad97684e37f
Ernest Ho <ernest.heh@gmail.com>
2023-03-09 16:38:47 +0800

    added function to create a bookmark, with a Movie/List pair

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,5 +1,32 @@
 class BookmarksController < ApplicationController
+  before_action :set_list, only: %i[new create destroy]
+
   def new
     @bookmark = Bookmark.new
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.list = @list
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render 'bookmarks/new', status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @bookmark.destroy
+    redirect_to list_path(@list)
+  end
+
+  private
+
+  def set_list
+    @list = List.find(params[:list_id])
+  end
+
+  def bookmark_params
+    params.require(:bookmark).permit(:movie_id, :list_id, :comment)
   end
 end
--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -11,9 +11,22 @@ class ListsController < ApplicationController
     @list = List.new
   end
 
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render 'lists/new', status: :unprocessable_entity
+    end
+  end
+
   private
 
   def set_list
     @list = List.find(params[:id])
+  end
+
+  def list_params
+    params.require(:list).permit(:name)
   end
 end
