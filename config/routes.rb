0122e1e83a5aaec07bbb67c819d25b55b6fcf5e2
Ernest Ho <ernest.heh@gmail.com>
2023-03-09 15:23:49 +0800

    added routes for lists: index, show, new, create

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,14 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :lists, only: %i[index show new create]
 end
+
+
+# A user can see all the lists
+# GET "lists"
+# A user can see the details of a given list and its name
+# GET "lists/42"
+# A user can create a new list
+# GET "lists/new"
+# POST "lists"
