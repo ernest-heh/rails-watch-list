2ccab7d0d713bc58e058403187e1e288710d4c28
Ernest Ho <ernest.heh@gmail.com>
2023-03-09 15:42:34 +0800

    added action for lists controller to display and add a new list

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,5 +1,19 @@
 class ListsController < ApplicationController
+  before_action :set_list, only: %i[show]
+
   def index
     @lists = List.all
+  end
+
+  def show; end
+
+  def new
+    @list = List.new
+  end
+
+  private
+
+  def set_list
+    @list = List.find(params[:id])
   end
 end
